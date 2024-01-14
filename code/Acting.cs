e93688eba23c8db183e4c53e138bb5d23a93d2ce
yuberee <59583743+yuberee@users.noreply.github.com>
2024-01-14 21:50:48 +0100

    Add item holding options

--- code/Acting.cs
+++ code/Acting.cs
@@ -4,17 +4,60 @@ using Sandbox.Citizen;
 public sealed class Acting : Component
 {
 	[Property]
+	public SkinnedModelRenderer Actor { get; set; }
+
+	[Property]
 	public CitizenAnimationHelper AnimationHelper { get; set; }
 
 	[Property]
 	public CitizenAnimationHelper.HoldTypes HoldType { get; set; } = CitizenAnimationHelper.HoldTypes.None;
 
 	[Property]
+	public Model HoldingLeft { get; set; }
+
+	[Property]
+	public Angles LeftAngleOffset { get; set; }
+
+	[Property]
+	public Vector3 LeftPositionOffset { get; set; }
+
+	[Property]
+	public Model HoldingRight { get; set; }
+
+	[Property]
+	public Angles RightAngleOffset { get; set; }
+
+	[Property]
+	public Vector3 RightPositionOffset { get; set; }
 
 	protected override void OnUpdate()
 	{
 		if ( AnimationHelper == null ) return;
+		if ( Actor == null ) return;
 
 		AnimationHelper.HoldType = HoldType;
+
+		var draw = Gizmo.Draw;
+
+		if ( HoldingLeft != null )
+		{
+			var handTransform = Actor.GetBoneObject( 15 ).Transform.World;
+			var offsetTransform = handTransform.WithPosition( handTransform.PointToWorld( LeftPositionOffset ) );
+			var rotatedTransform = offsetTransform.WithRotation( offsetTransform.Rotation * LeftAngleOffset );
+			draw.Model( HoldingLeft, rotatedTransform );
+		}
+
+		if ( HoldingRight != null )
+		{
+			var handTransform = Actor.GetBoneObject( 10 ).Transform.World;
+			var offsetTransform = handTransform.WithPosition( handTransform.PointToWorld( RightPositionOffset ) );
+			var rotatedTransform = offsetTransform.WithRotation( offsetTransform.Rotation * RightAngleOffset );
+			draw.Model( HoldingRight, rotatedTransform );
+		}
+	}
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
 	}
 }
